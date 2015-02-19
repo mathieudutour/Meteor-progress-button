4db14b6d582efdbfd9f68f25dc546de5e039639e
Mathieu Dutour <mathieu@dutour.me>
2015-02-19 15:30:28 +0000

    add git url

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'matdutour:progress-button',
   version: '0.1.0',
   summary: 'Easy to use helper to display a nifty progress button',
-  git: '',
+  git: 'https://github.com/mathieudutour/Meteor-progress-button.git',
   documentation: 'README.md'
 });
 
