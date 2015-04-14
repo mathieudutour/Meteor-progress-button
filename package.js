afca549d5b0e4d31b8ef4e34963e7bf3f821c429
Mathieu Dutour <mathieu@dutour.me>
2015-04-14 11:55:28 +0100

    fix typo

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'matdutour:progress-button',
-  version: '0.1.13',
+  version: '0.1.14',
   summary: 'Easy to use helper to display a nifty progress button',
   git: 'https://github.com/mathieudutour/Meteor-progress-button.git',
   documentation: 'README.md'
